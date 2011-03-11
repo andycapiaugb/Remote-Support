0e5b5da6c3442aa08fcd4931d1c721d750818304
Andy Capiau <andy.capiau@geraardsbergen.be>
2011-03-11 10:06:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RemoteSupport::Application.config.secret_token = 'd14b38d4a07658a8755aecbaf6f9e084e1cccd14747dfd63d1e5807ee815294b92725ec2d5f7d00ced75867ddadb0e06338be5c5c18c4fcca145331067781002'
